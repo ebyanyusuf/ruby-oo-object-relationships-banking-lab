2d36c6ca930287ed264c0a20dc652ba046d72278
ebyanyusuf <ebyan.yusuf@gmail.com>
2020-11-30 16:04:13 -0500

    commit tree ebff0c3aedd3069e8ca14592ded1aa74dffc1b81

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+ 
+  def initialize(sender, receiver, amount)
+  
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+
+ end
+
+ def valid?
+  sender.valid? && receiver.valid?
+ end
+
+
+
+
+
 end
